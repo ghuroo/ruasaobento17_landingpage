ee345ac7d79e32308a5bc922a5354e9a20d281ad
Daniel Neves <ghuroo@gmail.com>
2017-05-28 22:53:25 +0100

    frontend slider updated

--- assets/source/javascript/main.js
+++ assets/source/javascript/main.js
@@ -31,6 +31,11 @@ $('.banner .button').on('click',function() {
         },
     });
 
+    swiper.appendSlide([
+        '<div class="swiper-slide"><div class="va-parent"><div class="va-child"><img src="./images/_placeholders/slider-1.jpg" /></div></div></div>',
+        '<div class="swiper-slide"><div class="va-parent"><div class="va-child"><img src="./images/_placeholders/slider-1.jpg" /></div></div></div>'
+    ]);
+
 });
 
 $('.banner .close').on('click',function() {
